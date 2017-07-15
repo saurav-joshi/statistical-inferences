62f85a884f81afdf89680329f2a6c02e3d8559e0
saurav-joshi <sauravjoshi2000@yahoo.com>
2017-07-15 22:16:56 +0800

    Add files via upload

---
+++ nytimes_pull.py
@@ -0,0 +1,30 @@
+import urllib
+import json
+
+def main(api_key, category, label):
+    
+    content = []
+    for i in range(0,5):
+        # print "http://api.nytimes.com/svc/search/v2/articlesearch.json?fq=news_desk:('%s')&api-key=%s&page=%s" % (category, api_key, i)
+        h = urllib.urlopen("http://api.nytimes.com/svc/search/v2/articlesearch.json?fq=news_desk:(\"%s\")&api-key=%s&page=%s" % (category, api_key, i))
+        print (h)
+        try:
+            result = json.loads(h.read())
+            content.append(result)
+        except ValueError:
+            print ("Malformed JSON: ")
+            continue #In the rare cases that JSON refuses to parse
+
+    f = open(label, 'w')
+    for line in content:
+        try:
+            f.write('%s\n' % line)
+        except UnicodeEncodeError:
+            pass
+            
+    f.close()
+
+if __name__ == '__main__':
+    main("f7b4a1749764aec0364b215c354e3a0f:18:25759498", "Arts","arts")
+    main("f7b4a1749764aec0364b215c354e3a0f:18:25759498", "Sports","sports")
+
